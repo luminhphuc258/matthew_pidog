bc238c2f66dba1906af6b473e19aedaf20503fa3
luminhphuc258 <luminhphuc258@gmail.com>
2026-01-01 13:22:20 -0600

    update new face 3d services v13

--- doreamonservice.py
+++ doreamonservice.py
@@ -105,7 +105,7 @@ def parse_args():
     parser.add_argument("--windowed", action="store_true")
     parser.add_argument("--size", default="800x600")
     parser.add_argument("--port", type=int, default=39393)
-    parser.add_argument("--talk-interval", type=float, default=2.0)
+    parser.add_argument("--talk-interval", type=float, default=1.5)
     parser.add_argument("--fade", type=float, default=0.2)
     parser.add_argument("--talk-overlay-alpha", type=int, default=60)
     parser.add_argument("--talk-overlay-duration", type=float, default=0.12)
